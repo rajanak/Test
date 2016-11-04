1173f75da92d631372ff099296442b478747ba1d
rajanak <rajanakk123@gmail.com>
2016-11-04 16:27:00 +0530

    changes are made

--- plink.rb
+++ plink.rb
@@ -4,34 +4,25 @@ require 'nokogiri'
 require 'open-uri'
 require 'optparse'
 require 'uri'
-options={:website=>nil}
+options={}
 opt_parser = OptionParser.new do |opts|
-  opts.banner = "Usage: opt.rb [options]"
-  opts.on("-n", "--website website", "website") do |website|
-    options[:website]=website;
+	opts.banner = "Usage: opt.rb [options]"
+	opts.on("-n", "--website website", "website") do |website|
+	    options[:website]=website
+	end    
+end
+opt_parser.parse!
+strurl=options[:website]
+page = Nokogiri::HTML(open(strurl))   
+puts "title:\t"+ page.css('title').text
+['h1', 'h2', 'h3' , 'h4'].each { |tag| puts tag + ':' + page.css(tag).text }
+l=page.css('a').map do |link| 
+	if (l = link.attr("href")) && !l.empty?
+      URI::join(strurl, l)
     end
-    end
-    opt_parser.parse!
-    page = Nokogiri::HTML(open(options[:website]))
-    puts "title:\t"+ page.css('title').text
-    puts "h1:"+page.css('h1').text
-    puts "h2:"+page.css('h2').text
-    puts "h3:"+page.css('h3').text
-    puts "h4:"+page.css('h4').text
-    l=page.css('a').map { |link| link['href'] }
-    url= l.uniq
-    n=url.size
-    puts"extrnallink:"
-    (0..n).each do  |i|
-      uri = URI.parse(URI.encode(url[i].to_s))
-      if(uri.scheme!=nil)
-        puts uri
-        end
-        end
-        puts "internallink:"
-        (0..n).each do  |i|
-          uri = URI.parse(url[i].to_s)
-          if(uri.scheme==nil)
-            puts uri
-            end
-          end
+end.compact.uniq
+n=l.size
+puts"extrnallink:"
+puts l.reject{|n| n.to_s.include?strurl}
+puts"internallink:" 
+puts l.select{|n| n.to_s.include?strurl}
